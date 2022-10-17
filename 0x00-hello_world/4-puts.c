f42cacc496d0731ecefc7bf7fa95f2984ff138d4
charlesnzisa <charlesnzisa1993@gmail.com>
2022-10-17 05:25:03 -0700

    Task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,22 +3,44 @@
 
 
 
+
+
+
+
 /**
-
- * main - Entry point
-
  *
+ *
+ *
+ *  * main - Entry point
+ *
+ *
+ *
+ *   *
+ *
+ *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *
+ *
+ *     */
 
- * Return: Always 0 (Success)
 
- */
 
 int main(void)
 
+
+
 {
 
-puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+
+	return (0);
+
+
 
 }
