1c9f59910fceb84cbd68c354e750777109fe4d53
charlesnzisa <charlesnzisa1993@gmail.com>
2022-10-17 09:10:09 -0700

    my first line of c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,46 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
-
-
-
-
-
-
-
-/**
+/*
+ * main - print a line of code using puts
  *
- *
- *
- *  * main - Entry point
- *
- *
- *
- *   *
- *
- *
- *
- *    * Return: Always 0 (Success)
- *
- *
- *
- *     */
-
-
-
+ * Return: 0
+*/
 int main(void)
-
-
-
 {
-
-
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-
-
 	return (0);
-
-
-
 }
