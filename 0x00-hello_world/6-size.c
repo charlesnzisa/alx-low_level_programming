e8f55c82fc3e408de6f9e59f29a24e750aa38dc7
charlesnzisa <charlesnzisa1993@gmail.com>
2022-10-20 00:16:04 -0700

    My first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Write a C program that prints the size of various types on the computer it is compiled and run on
+ * main - Write a C program that prints the size of various type
  *
  * Return: 0
  */
