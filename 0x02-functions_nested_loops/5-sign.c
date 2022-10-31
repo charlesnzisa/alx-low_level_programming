0a2f6af201c57ec01e52a6b036a816711170f270
charlesnzisa <charlesnzisa1993@gmail.com>
2022-10-31 04:08:25 -0700

    Task 5 commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,14 +8,12 @@
  *         0 if the number is zero,
  *         -1 if the number is less than zero.
  */
-
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-
 	}
 	else if (n == 0)
 	{
@@ -23,12 +21,8 @@ int print_sign(int n);
 		return (0);
 	}
 	else
-		(n < 0)
 	{
 		_putchar('-');
 		return (-1);
-
 	}
 }
-
-
