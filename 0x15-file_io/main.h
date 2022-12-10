73becbf8c0481cc6c0254d4862a8d4876dc502c4
charlesnzisa <charlesnzisa1993@gmail.com>
2022-12-10 00:06:22 -0800

    My first commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistad.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
